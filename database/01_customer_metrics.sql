8e6981bbf7ea59c63f6ddcde1e6ca4319bed2a9f
Khanim Abdullayeva <xabdullayeva775@gmail.com>
2026-09-24 23:02:10 +0400

    Update SQL File

--- database/01_customer_metrics.sql
+++ database/01_customer_metrics.sql
@@ -1,7 +1,7 @@
 SELECT COUNT(*) AS 'Total Customers'
 FROM telecom_churn;
 
--- Number of Total Customers
+-- Number of Total Customers : 3333
 
 SELECT Churn AS 'Churned Customers', COUNT(*) as Count
 FROM telecom_churn
@@ -9,3 +9,10 @@ GROUP BY Churn
 HAVING Churn = '1';
 
 -- Number of Customers Churned : 483
+
+SELECT Churn AS 'Churned Customers', COUNT(*) as Count
+FROM telecom_churn
+GROUP BY Churn
+HAVING Churn = '0';
+
+-- Nuber of Retained Customers : 2850
